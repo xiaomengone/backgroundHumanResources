4e73ce41c2a64dcff7e30481a14ca35e2007dff4
xiaomeng <764504466@qq.com>
2023-09-18 10:13:47 +0800

    featrue: 0918发布1

--- vue.config.js
+++ vue.config.js
@@ -25,6 +25,7 @@ module.exports = {
    * Detail: https://cli.vuejs.org/config/#publicpath
    */
   publicPath: "/",
+  base: "/backgroundHumanResources/dist",
   outputDir: "dist",
   assetsDir: "static",
   lintOnSave: process.env.NODE_ENV === "development",
